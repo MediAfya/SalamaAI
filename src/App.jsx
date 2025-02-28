e451380cd80bc5b0ae833d5c8307678e991abf7e
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-28 22:35:07 +0300

    Added React Router routes for navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 const App = () => {
-    return (
-        <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4 ">
-            <div className="relative mr-10 hidden sm:flex">
-                {/* Sidebar component */}
-            </div>
+  return (
+    <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
+      <div className="relative mr-10 hidden sm:flex">
+        {/* Sidebar component */}
+      </div>
 
-            <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
-                {/* Navbar component */}
+      <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
+        {/* Navbar component */}
 
-                
-            </div>
-        </div>
-    )
-}
+        <Routes>
+          <Route path="/" element={<Home Page />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/medical-records" element={<MedicalRecords />} />
+          <Route
+            path="/medical-records/:id"
+            element={<SingleRecordDetails />}
+          />
+          <Route path="/screening-schedules" element={<ScreeningSchedule />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
