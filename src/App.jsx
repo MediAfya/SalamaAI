af42ad245fce4b7b6fdc8cdbd6d095ec5d325fca
mwangimuringi <mwangimuringi10@gmail.com>
2025-02-28 09:35:33 +0300

    Refactor App layout with flexbox and dark theme

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4 ">
+            <div className="relative mr-10 hidden sm:flex">
+                {/* Sidebar component */}
+            </div>
+
+            <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
+                {/* Navbar component */}
+
+                
+            </div>
+        </div>
+    )
+}
