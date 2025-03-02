c9d6e07fcaa1cb1352be76f1fba689502dca2b77
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-02 22:21:57 +0300

    Added metric calculations for screenings and personalized treatments

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { DisplayCampaigns } from "../components";
+
+
 const DisplayInfo = ({ title, info }) => {
   return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={data}
+    />
     <div className="flex flex-col gap-4 w-full">
       <h1 className="font-bold text-[24px] leading-[36px] text-white">
         {title}
@@ -9,6 +17,15 @@ const DisplayInfo = ({ title, info }) => {
       <p className="font-bold text-[16px] leading-[26px] text-white">
         {info}
       </p>
+      <div className="flex flex-col gap-4 w-full">
+        <h1 className="font-bold text-[24px] leading-[36px] text-white">
+          {title}
+        </h1>
+        <p className="font-bold text-[16px] leading-[26px] text-white">
+          {info}
+        </p>
+      </div>
+    </>
     </div>
   );
 };
