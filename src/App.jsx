733e77199b8b521d6dd8f28e20fe7367149eaef2
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-04 09:51:16 +0300

    Integrated Privy Authentication & Improved Routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Sidebar, Navbar } from "./components";
+import { Home, Profile } from "./pages";
+import MedicalRecords from "./pages/records/index";
+import ScreeningSchedule from "./pages/ScreeningSchedule";
+import SingleRecordDetails from "./pages/records/single-record-details";
+import { usePrivy } from "@privy-io/react-auth";
 
 const App = () => {
+  const { ready, authenticated, login, user, logout } = usePrivy();
+  const navigate = useNavigate();
+  // console.log(authenticated, user);
+  // useEffect(() => {
+  //   if (!authenticated) {
+  //     login();
+  //   }
+  // }, [authenticated, login]);
+
+  // if (!ready) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (!authenticated) {
+  //   return <div className="bg-[#13131a]"></div>;
+  // }
+
   return (
-    <div className="relative flex min-h-screen flex-row bg-[#13131a] p-4">
-      <div className="relative mr-10 hidden sm:flex">
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
       </div>
 
-      <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
+
         <Routes>
-          <Route path="/" element={<Home Page />} />
+          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/medical-records" element={<MedicalRecords />} />
           <Route
