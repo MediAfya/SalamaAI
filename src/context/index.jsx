19943a24154abdd827cec3b687dc49bdd2f6e7f5
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-05 22:23:22 +0300

    Added functions to create and update users and records.

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -38,8 +38,81 @@ export const StateContextProvider = ({ children }) => {
     }
   }, []);
 
+  // Function to create a new user
+  const createUser = useCallback(async (userData) => {
+    try {
+      const newUser = await db
+        .insert(Users)
+        .values(userData)
+        .returning({ id: Users.id, createdBy: Users.createdBy })
+        .execute();
+      setUsers((prevUsers) => [...prevUsers, newUser[0]]);
+      return newUser[0];
+    } catch (error) {
+      console.error("Error creating user:", error);
+      return null;
+    }
+  }, []);
+
+  // Function to fetch all records for a specific user
+  const fetchUserRecords = useCallback(async (userEmail) => {
+    try {
+      const result = await db
+        .select()
+        .from(Records)
+        .where(eq(Records.createdBy, userEmail))
+        .execute();
+      setRecords(result);
+    } catch (error) {
+      console.error("Error fetching user records:", error);
+    }
+  }, []);
+
+  // Function to create a new record
+  const createRecord = useCallback(async (recordData) => {
+    try {
+      const newRecord = await db
+        .insert(Records)
+        .values(recordData)
+        .returning({ id: Records.id })
+        .execute();
+      setRecords((prevRecords) => [...prevRecords, newRecord[0]]);
+      return newRecord[0];
+    } catch (error) {
+      console.error("Error creating record:", error);
+      return null;
+    }
+  }, []);
+
+  const updateRecord = useCallback(async (recordData) => {
+    try {
+      const { documentID, ...dataToUpdate } = recordData;
+      console.log(documentID, dataToUpdate);
+      const updatedRecords = await db
+        .update(Records)
+        .set(dataToUpdate)
+        .where(eq(Records.id, documentID))
+        .returning();
+    } catch (error) {
+      console.error("Error updating record:", error);
+      return null;
+    }
+  }, []);
+
   return (
-    <StateContext.Provider value={{ users, records, fetchUsers, fetchUserByEmail, currentUser }}>
+    <StateContext.Provider
+      value={{
+        users,
+        records,
+        fetchUsers,
+        fetchUserByEmail,
+        createUser,
+        fetchUserRecords,
+        createRecord,
+        currentUser,
+        updateRecord,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
