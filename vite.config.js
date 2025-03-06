21480751ffdb548eb46b0a2659c664a6d7e53015
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-06 14:37:57 +0300

    Updated Vite config for alias and polyfills; add database configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//       // Add the alias for crypto-browserify
+//       crypto: path.resolve(__dirname, "node_modules/crypto-browserify"),
+//     },
+//   },
+//   base: "./",
+// });
+
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -7,7 +23,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      crypto: path.resolve(__dirname, "node_modules/crypto-browserify"),
     },
   },
   base: "./",
+  define: {
+    global: "window", // Polyfill global as window
+  },
+  optimizeDeps: {
+    include: ["crypto-browserify", "randombytes"],
+  },
 });
