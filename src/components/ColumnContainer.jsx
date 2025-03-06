2f066cdf9c3d9aff7f60d287ac7e40fed7fb2301
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-06 12:13:09 +0300

    Enabled column title editing and task actions.

--- src/components/ColumnContainer.jsx
+++ src/components/ColumnContainer.jsx
@@ -28,7 +28,10 @@ function ColumnContainer({
     isDragging,
   } = useSortable({
     id: column.id,
-    data: { type: "Column", column },
+    data: {
+      type: "Column",
+      column,
+    },
     disabled: editMode,
   });
 
@@ -38,40 +41,78 @@ function ColumnContainer({
   };
 
   if (isDragging) {
-    return <div ref={setNodeRef} style={style} className="dragging-style"></div>;
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="flex h-[500px] max-h-[500px] w-[350px] flex-col rounded-md border-2 border-pink-500 bg-[#1c1c24] opacity-40"
+      ></div>
+    );
   }
 
   return (
-    <div ref={setNodeRef} style={style} className="column-container">
-      {/* Column Title and Edit Mode */}
-      <div {...attributes} {...listeners} onClick={() => setEditMode(true)} className="column-header">
-        {!editMode && column.title}
-        {editMode && (
-          <input
-            value={column.title}
-            onChange={(e) => updateColumn(column.id, e.target.value)}
-            autoFocus
-            onBlur={() => setEditMode(false)}
-            onKeyDown={(e) => { if (e.key === "Enter") setEditMode(false); }}
-          />
-        )}
-        <button onClick={() => deleteColumn(column.id)}>
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="flex h-[500px] max-h-[500px] w-[350px] flex-col rounded-xl bg-[#1c1c24]"
+    >
+      <div
+        {...attributes}
+        {...listeners}
+        onClick={() => {
+          setEditMode(true);
+        }}
+        className="text-md m-2 flex h-[60px] cursor-grab items-center justify-between rounded-xl bg-[#13131a] p-3 font-bold"
+      >
+        <div className="flex gap-2">
+          {!editMode && column.title}
+          {editMode && (
+            <input
+              className="rounded border bg-black px-2 outline-none focus:border-green-500"
+              value={column.title}
+              onChange={(e) => updateColumn(column.id, e.target.value)}
+              autoFocus
+              onBlur={() => {
+                setEditMode(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key !== "Enter") return;
+                setEditMode(false);
+              }}
+            />
+          )}
+        </div>
+        <button
+          onClick={() => {
+            deleteColumn(column.id);
+          }}
+          className="rounded stroke-gray-500 px-1 py-2 hover:bg-columnBackgroundColor hover:stroke-white"
+        >
           <IconTrash />
         </button>
       </div>
 
-      {/* Task List */}
-      <div className="task-list">
+      <div className="flex flex-grow flex-col gap-4 overflow-y-auto overflow-x-hidden p-2">
         <SortableContext items={tasksIds}>
           {tasks.map((task) => (
-            <TaskCard key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
+            <TaskCard
+              key={task.id}
+              task={task}
+              deleteTask={deleteTask}
+              updateTask={updateTask}
+            />
           ))}
         </SortableContext>
       </div>
 
-      {/* Add Task Button */}
-      <button onClick={() => createTask(column.id)} className="add-task-button">
-        <IconPlus /> Add Task
+      <button
+        className="flex items-center gap-2 rounded-md border-2 border-columnBackgroundColor border-x-columnBackgroundColor p-4 hover:bg-mainBackgroundColor hover:text-green-500 active:bg-black"
+        onClick={() => {
+          createTask(column.id);
+        }}
+      >
+        <IconPlus />
+        Add task
       </button>
     </div>
   );
