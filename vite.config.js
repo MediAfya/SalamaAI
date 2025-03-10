691fb8075022a7faf5680970075937324fc11cf7
mwangimuringi <mwangimuringi10@gmail.com>
2025-03-10 18:51:25 +0300

    🐛 Fix path issues for GitHub Pages and localhost

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// import path from "path";
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//       // Add the alias for crypto-browserify
-//       crypto: path.resolve(__dirname, "node_modules/crypto-browserify"),
-//     },
-//   },
-//   base: "./",
-// });
-
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
